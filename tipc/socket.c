0f964b2d4db1a746d508f725c0b136fc4c01dfa1
Richard Alpe <richard.alpe@ericsson.com>
2014-09-18 13:48:04 +0200

    tipc: redesign socket list
    To accommodate kernel space changes we no longer reassemble
    publications for the same socket. We now ask for a list of sockets
    with TIPC_NL_SOCK_GET and if a socket has publications
    (TIPC_NLA_SOCK_HAS_PUBL) we ask for them using TIPC_NL_PUBL_GET.
    
    This is fairly slow for the worst case which is a high amount of
    published sockets with only one or a few publications.

--- tipc/socket.c
+++ tipc/socket.c
@@ -41,11 +41,69 @@
 
 ADD_SUBCMD(&cmd_list, cmd_sock, socket, "Show sockets");
 
+static int publ_print(struct nl_msg *nl_msg, void *arg)
+{
+	struct nlmsghdr *nlh = nlmsg_hdr(nl_msg);
+	struct nlattr *info[TIPC_NLA_MAX + 1];
+	struct nlattr *attrs[TIPC_NLA_PUBL_MAX + 1];
+
+	genlmsg_parse(nlh, 0, info, TIPC_NLA_MAX, NULL);
+
+	if (!info[TIPC_NLA_PUBL]) {
+		log_err("no publ received from kernel\n");
+		return NL_STOP;
+	}
+
+	if (nla_parse_nested(attrs, TIPC_NLA_PUBL_MAX,
+			     info[TIPC_NLA_PUBL], NULL)) {
+		log_err("parsing nested publ attributes\n");
+		return NL_STOP;
+	}
+
+	printf("  bound to {%u,%u,%u}\n",
+	       nla_get_u32(attrs[TIPC_NLA_PUBL_TYPE]),
+	       nla_get_u32(attrs[TIPC_NLA_PUBL_LOWER]),
+	       nla_get_u32(attrs[TIPC_NLA_PUBL_UPPER]));
+
+	return NL_OK;
+}
+
+static int publ_list(__u32 sock)
+{
+	int err;
+	struct tipc_nl_msg msg;
+	struct nlattr *attrs;
+
+	msg.nl_flags = NLM_F_REQUEST | NLM_F_DUMP;
+	msg.nl_cmd = TIPC_NL_PUBL_GET;
+	msg.callback = &publ_print;
+
+	err = msg_init(&msg);
+	if (err)
+		return err;
+
+	attrs = nla_nest_start(msg.nl_msg, TIPC_NLA_SOCK);
+	NLA_PUT_U32(msg.nl_msg, TIPC_NLA_SOCK_REF, sock);
+	nla_nest_end(msg.nl_msg, attrs);
+
+	err = msg_send(&msg);
+	if (err)
+		return err;
+
+	err = msg_recv(&msg, NULL);
+	if (err)
+		return err;
+
+	return 0;
+
+nla_put_failure:
+	msg_abort(&msg);
+
+	return -2;
+}
+
 static int sock_print(struct nl_msg *nl_msg, void *arg)
 {
-	int i;
-	__u32 ref;
-	__u32 *last_ref = arg;
 	struct nlmsghdr *nlh = nlmsg_hdr(nl_msg);
 	struct nlattr *info[TIPC_NLA_MAX + 1];
 	struct nlattr *attrs[TIPC_NLA_SOCK_MAX + 1];
@@ -56,26 +114,13 @@ static int sock_print(struct nl_msg *nl_msg, void *arg)
 		log_err("no socket received from kernel\n");
 		return NL_STOP;
 	}
-
-	if (nla_parse_nested(attrs, TIPC_NLA_SOCK_MAX,
-			     info[TIPC_NLA_SOCK], NULL)) {
+	if (nla_parse_nested(attrs, TIPC_NLA_SOCK_MAX, info[TIPC_NLA_SOCK],
+			     NULL)) {
 		log_err("parsing nested socket attributes\n");
 		return NL_STOP;
 	}
+	printf("socket %u\n", nla_get_u32(attrs[TIPC_NLA_SOCK_REF]));
 
-	if (!attrs[TIPC_NLA_SOCK_REF]) {
-		log_err("no socket name from kernel\n");
-		return NL_STOP;
-	}
-
-	ref = nla_get_u32(attrs[TIPC_NLA_SOCK_REF]);
-
-	if (!*last_ref || ref != *last_ref) {
-		*last_ref = ref;
-		printf("socket %u\n", ref);
-	}
-
-	/* A connected socket can't have publications */
 	if (attrs[TIPC_NLA_SOCK_CON]) {
 		__u32 node;
 		struct nlattr *con[TIPC_NLA_CON_MAX + 1];
@@ -97,23 +142,8 @@ static int sock_print(struct nl_msg *nl_msg, void *arg)
 				nla_get_u32(con[TIPC_NLA_CON_INST]));
 		else
 			printf("\n");
-	} else if (attrs[TIPC_NLA_SOCK_PUBL]) {
-		struct nlattr *p;
-
-		nla_for_each_nested(p, attrs[TIPC_NLA_SOCK_PUBL], i) {
-			struct nlattr *publ[TIPC_NLA_PUBL_MAX + 1];
-
-			if (nla_parse_nested(publ, TIPC_NLA_PUBL_MAX, p,
-					     NULL)) {
-				log_err("parsing nested publication info\n");
-				return NL_STOP;
-			}
-
-			printf("  bound to {%u,%u,%u}\n",
-			       nla_get_u32(publ[TIPC_NLA_PUBL_TYPE]),
-			       nla_get_u32(publ[TIPC_NLA_PUBL_LOWER]),
-			       nla_get_u32(publ[TIPC_NLA_PUBL_UPPER]));
-		}
+	} else if (attrs[TIPC_NLA_SOCK_HAS_PUBL]) {
+		publ_list(nla_get_u32(attrs[TIPC_NLA_SOCK_REF]));
 	}
 
 	return NL_OK;
@@ -131,11 +161,9 @@ static int list(struct cmd_struct *cmd, struct arg_struct *arg)
 	err = msg_init(&msg);
 	if (err)
 		return err;
-
 	err = msg_send(&msg);
 	if (err)
 		return err;
-
 	err = msg_recv(&msg, &last_ref);
 	if (err)
 		return err;
